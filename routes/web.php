bf180b3d97eb8cd22fe9e8300d00a86a6101a99c
jds-11 <jaydenramkisoen@gmail.com>
2025-06-18 14:03:33 +0200

    niks

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
+<<<<<<< Updated upstream
 Route::get('/', [DashboardController::class, 'home'])->name('dashboard.home');
 Route::get('/analyse', [DashboardController::class, 'analyse'])->name('dashboard.analyse');
 Route::get('/energiebespaar', [DashboardController::class, 'energiebespaar'])->name('dashboard.energiebespaar');
 Route::get('/instellingen', [DashboardController::class, 'instellingen'])->name('dashboard.instellingen');
+=======
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+>>>>>>> Stashed changes
