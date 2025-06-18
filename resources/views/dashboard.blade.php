7d18119aa0ff935f546884cd374bb22b1fa32f87
Milan R <github@milanramakers.nl>
2025-06-18 11:14:55 +0200

    Eerste commit: Laravel project basis en database connectie

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Mijn Dashboard</title>
+</head>
+<body>
+    <h1>Welkom op je eigen dashboard!</h1>
+    <p>Dit is jouw startpagina, helemaal zonder Laravel welcome screen.</p>
+</body>
+</html>
